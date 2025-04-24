1bb691c49055c94c2bafc4d8894691b4eda924e7
João Vitor Cezario Sanches <joaovitormanolo@gmail.com>
2025-04-24 14:26:24 -0300

    fix?: Refactors socket service part 1

---
+++ src/controller/socket.ts
@@ -0,0 +1,106 @@
+import { Server as HTTPServer } from "http";
+import { Server as SocketIOServer, Socket } from "socket.io"
+import { SOCKET_CLIENT_EVENTS, SOCKET_SERVER_EVENTS, User } from "../shared/main";
+import RoomService from '../service/RoomService'
+
+type Handler = (socket: Socket, params: any, callback?: (callbackParams: any) => void) => void;
+type Emitter = (params: any) => void
+
+class SocketServer {
+    private static _instance: SocketIOServer;
+
+    private static eventHandlers: Record<SOCKET_SERVER_EVENTS, Handler> = {
+        [SOCKET_SERVER_EVENTS.DISCONNECT]: this._onDisconnect,
+        [SOCKET_SERVER_EVENTS.USER_JOIN]: this._onUserJoin,
+        [SOCKET_SERVER_EVENTS.USER_LEAVE]: this._onUserLeave,
+    };
+
+    private static eventEmitters: Record<SOCKET_CLIENT_EVENTS, Emitter> = {
+        [SOCKET_CLIENT_EVENTS.USER_JOINED]: ({ roomId, player }: { roomId: string, player: User }) => this.io.to(roomId).emit(SOCKET_CLIENT_EVENTS.USER_JOINED, player),
+        [SOCKET_CLIENT_EVENTS.USER_LEFT]: ({ roomId, player }: { roomId: string, player: string }) => this.io.to(roomId).emit(SOCKET_CLIENT_EVENTS.USER_LEFT, player)
+    }
+
+    static init(httpServer: HTTPServer): SocketIOServer {
+        if (!this._instance) {
+            this._instance = new SocketIOServer(httpServer, {
+                cors: {
+                    origin: "*",
+                    methods: ["GET", "POST"]
+                }
+            });
+
+            this._instance.on("connection", this._onConnection.bind(this));
+        }
+
+        return this._instance;
+    }
+
+    private static get io(): SocketIOServer {
+        if (!this._instance) throw new Error("SocketServer not initialized.");
+        return this._instance;
+    }
+
+    private static emit<E extends SOCKET_CLIENT_EVENTS>(
+        event: E,
+        data: Parameters<typeof this.eventEmitters[E]>[0]
+    ) {
+        this.eventEmitters[event]?.(data);
+    }
+
+    private static _onConnection(socket: Socket) {
+        Object.entries(this.eventHandlers).forEach(([key, handler]) =>
+            socket.on(key, (params, callback) =>
+                handler.call(this, socket, params, callback))
+        )
+    }
+
+    private static _onUserJoin(socket: Socket, {
+        roomId,
+        playerName,
+        peerId,
+        summonerId,
+    }: {
+        roomId: string;
+        playerName: string;
+        peerId: string;
+        summonerId: string;
+    }, callback?: (players: User[]) => void) {
+        const room = RoomService.addPlayerToRoom(
+            roomId,
+            socket.id,
+            playerName,
+            peerId,
+            summonerId
+        );
+        if (room) {
+            socket.join(roomId);
+            let players = RoomService.getRoomPlayers(roomId);
+            callback?.(players)
+            socket.to(roomId).emit(SOCKET_CLIENT_EVENTS.USER_JOINED, {
+                roomId, player: {
+                    id: socket.id,
+                    peerId,
+                    summonerId,
+                    playerName,
+                }
+            });
+        } else {
+            socket.emit("error", { message: "Room does not exist" });
+        }
+    }
+
+    private static _onUserLeave(socket: Socket, { roomId, playerName }: { roomId: string; playerName: string }) {
+        RoomService.removePlayer(roomId, playerName);
+        socket.leave(roomId);
+    }
+
+    private static _onDisconnect(socket: Socket) {
+        const [disconnectedPlayer, roomId] = RoomService.removePlayerBySocket(
+            socket.id
+        );
+        if (!disconnectedPlayer || !roomId) return;
+        this.emit(SOCKET_CLIENT_EVENTS.USER_LEFT, { roomId, disconnectedPlayer });
+    }
+}
+
+export default SocketServer;
--- src/server.ts
+++ src/server.ts
@@ -1,68 +1,15 @@
 import express from "express";
-import { Server as SocketIOServer, Socket } from "socket.io";
 import http from "http";
 import cors from "cors";
-import RoomService from "./service/RoomService";
+import SocketServer from "./controller/socket";
 
 const app = express();
 const server = http.createServer(app);
-const io = new SocketIOServer(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
 
 app.use(express.json());
 app.use(cors());
 
-io.on("connection", (socket: Socket) => {
-  socket.on(
-    "joinRoom",
-    ({
-      roomId,
-      playerName,
-      peerId,
-      summonerId,
-    }: {
-      roomId: string;
-      playerName: string;
-      peerId: string;
-      summonerId: string;
-    }) => {
-      const room = RoomService.addPlayerToRoom(
-        roomId,
-        socket.id,
-        playerName,
-        peerId,
-        summonerId
-      );
-      if (room) {
-        socket.join(roomId);
-        let players = RoomService.getRoomPlayers(roomId);
-        io.to(roomId).emit("userJoined", players);
-      } else {
-        socket.emit("error", { message: "Room does not exist" });
-      }
-    }
-  );
-
-  socket.on(
-    "leaveRoom",
-    ({ roomId, playerName }: { roomId: string; playerName: string }) => {
-      RoomService.removePlayer(roomId, playerName);
-      socket.leave(roomId);
-    }
-  );
-
-  socket.on("disconnect", () => {
-    const [disconnectedPlayer, roomId] = RoomService.removePlayerBySocket(
-      socket.id
-    );
-    if (!disconnectedPlayer || !roomId) return;
-    io.to(roomId).emit("playerDisconnected", disconnectedPlayer);
-  });
-});
+SocketServer.init(server)
 
 const PORT = 3001;
 server.listen(PORT, () => {
